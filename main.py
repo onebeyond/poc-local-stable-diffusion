507cc93ba12d5ee708a21df241a5a4cdc6442985
Jose V Giner (GentooXativa) <jvginer@gentooxativa.com>
2023-07-19 17:14:27 +0200

    added python and requeriment files

---
+++ main.py
@@ -0,0 +1,61 @@
+
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_bolt import App
+from diffusers import StableDiffusionPipeline
+import torch
+import os
+
+from dotenv import dotenv_values
+env_vars = dotenv_values('.env')
+
+if "MODEL_PATH" not in env_vars:
+    print("Please set the MODEL_PATH environment variable to the path of the model you want to use.")
+    exit(1)
+
+if "SLACK_BOT_AUTH_TOKEN" not in env_vars:
+    print("Please set the SLACK_BOT_AUTH_TOKEN environment variable to the token of the Slack bot you want to use.")
+    exit(1)
+
+if "SLACK_APP_AUTH_TOKEN" not in env_vars:
+    print("Please set the SLACK_APP_AUTH_TOKEN environment variable to the token of the Slack app you want to use.")
+    exit(1)
+
+model_id = env_vars["MODEL_PATH"]
+bot_user_id = env_vars["SLACK_BOT_USER_ID"]
+
+if "SLACK_BOT_USER_ID" not in env_vars:
+    print("Please set the SLACK_BOT_USER_ID environment variable to the user ID of the Slack bot you want to use.")
+    exit(1)
+
+app = App(token=env_vars["SLACK_BOT_AUTH_TOKEN"])
+
+
+@app.event("app_mention")
+def ask_for_introduction(client, event, ack):
+    user_id = event["user"]
+    text = event["text"]
+
+    if text.startswith(f"<@{bot_user_id}> draw me:"):
+        prompt = text[23:]
+        text = f"Hi <@{user_id}>! 🎉 I will try to draw and i will send the image.\nYou requested:```{prompt}```"
+        ack()
+        client.chat_postMessage(
+            channel=event["channel"],
+            text=text,
+            thread_ts=event["event_ts"]
+        )
+        pipe = StableDiffusionPipeline.from_pretrained(
+            model_id, torch_dtype=torch.float16)
+        pipe = pipe.to("cuda")
+        image = pipe(prompt).images[0]
+        file_path = f"/tmp/generated_image_{event['event_ts']}.png"
+        image.save(file_path)
+        client.files_upload(
+            channels=event['channel'],
+            thread_ts=event['event_ts'],
+            file=file_path
+        )
+
+
+if __name__ == "__main__":
+    SocketModeHandler(app, env_vars["SLACK_APP_AUTH_TOKEN"]).start()
